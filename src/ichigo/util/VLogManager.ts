786bdb5abd5749f3ebd17e524a790297268701fc
Kenichi Fukuda <kenichi.fukuda@mintjams.co.jp>
2025-10-08 03:58:48 +0900

    feat: enhance VLogger with dynamic log level checks and improved documentation

--- src/ichigo/util/VLogManager.ts
+++ src/ichigo/util/VLogManager.ts
@@ -3,22 +3,39 @@
 import { VLogger } from "./VLogger";
 import { LogLevel } from "./LogLevel";
 
+/**
+ * Manages loggers and their log levels.
+ */
 export class VLogManager {
+    /** The current log level. */
     #logLevel: LogLevel;
+    /** A map of logger instances by name. */
     #loggers: Map<string, VLogger> = new Map<string, VLogger>();
 
     constructor(logLevel: LogLevel = LogLevel.INFO) {
         this.#logLevel = logLevel;
     }
 
+    /**
+     * Sets the log level for all loggers.
+     * @param level The log level to set.
+     */
     set logLevel(level: LogLevel) {
         this.#logLevel = level;
     }
 
+    /**
+     * Gets the current log level.
+     */
     get logLevel(): LogLevel {
         return this.#logLevel;
     }
 
+    /**
+     * Gets a logger by name, creating it if it doesn't exist.
+     * @param name The name of the logger.
+     * @returns The logger instance.
+     */
     getLogger(name: string): VLogger {
         if (this.#loggers.has(name)) {
             return this.#loggers.get(name)!;
--- src/ichigo/util/VLogger.ts
+++ src/ichigo/util/VLogger.ts
@@ -3,8 +3,13 @@
 import { LogLevel } from "./LogLevel";
 import { VLogManager } from "./VLogManager";
 
+/**
+ * A simple logger class for virtual applications.
+ */
 export class VLogger {
+    /** The name of the logger. */
     #name: string;
+    /** The log manager instance. */
     #logManager: VLogManager;
 
     constructor(name: string, logManager: VLogManager) {
@@ -12,30 +17,67 @@ export class VLogger {
         this.#logManager = logManager;
     }
 
+    /**
+     * Indicates whether the debug level is enabled.
+     */
+    get isDebugEnabled(): boolean {
+        return [LogLevel.DEBUG].includes(this.#logManager.logLevel);
+    }
+
+    /**
+     * Indicates whether the info level is enabled.
+     */
+    get isInfoEnabled(): boolean {
+        return [LogLevel.DEBUG, LogLevel.INFO].includes(this.#logManager.logLevel);
+    }
+
+    /**
+     * Indicates whether the warn level is enabled.
+     */
+    get isWarnEnabled(): boolean {
+        return [LogLevel.DEBUG, LogLevel.INFO, LogLevel.WARN].includes(this.#logManager.logLevel);
+    }
+
+    /**
+     * Logs a debug message.
+     * @param message The message to log.
+     */
     debug(message: string): void {
-		if (![LogLevel.DEBUG].includes(this.#logManager.logLevel)) {
+		if (!this.isDebugEnabled) {
 			return;
 		}
 
         console.debug(`[${this.#name}] ${LogLevel.DEBUG}: ${message}`);
     }
 
+    /**
+     * Logs an info message.
+     * @param message The message to log.
+     */
     info(message: string): void {
-        if (![LogLevel.DEBUG, LogLevel.INFO].includes(this.#logManager.logLevel)) {
+        if (!this.isInfoEnabled) {
             return;
         }
 
         console.info(`[${this.#name}] ${LogLevel.INFO}: ${message}`);
     }
 
+    /**
+     * Logs a warn message.
+     * @param message The message to log.
+     */
     warn(message: string): void {
-        if (![LogLevel.DEBUG, LogLevel.INFO, LogLevel.WARN].includes(this.#logManager.logLevel)) {
+        if (!this.isWarnEnabled) {
             return;
         }
 
         console.warn(`[${this.#name}] ${LogLevel.WARN}: ${message}`);
     }
 
+    /**
+     * Logs an error message.
+     * @param message The message to log.
+     */
     error(message: string): void {
         console.error(`[${this.#name}] ${LogLevel.ERROR}: ${message}`);
     }
